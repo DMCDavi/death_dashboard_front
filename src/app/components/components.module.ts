215c7ec9f72799e579994910494e542394012717
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2021-10-13 22:24:42 -0300

    Adicionando table.component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,10 +5,11 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, NgbModule],
-  declarations: [FooterComponent, NavbarComponent],
-  exports: [FooterComponent, NavbarComponent]
+  declarations: [FooterComponent, NavbarComponent, TableComponent],
+  exports: [FooterComponent, NavbarComponent, TableComponent]
 })
 export class ComponentsModule {}
